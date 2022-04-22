9a5984952e2d6bf313e2b650e084cebcb7a87ab6
pearlrantolo@gmail.com <pearlrantolo@gmail.com>
2022-04-22 17:05:42 +0200

    Update

--- android/app/src/main/kotlin/com/example/onlineconsultation/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/onlineconsultation/MainActivity.kt
@@ -2,5 +2,6 @@ package com.example.onlineconsultation
 
 import io.flutter.embedding.android.FlutterActivity
 
+
 class MainActivity: FlutterActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onlineconsultation/screens/login.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
